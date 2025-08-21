6678cd15875bd63de5812c82f8c391fdff3cd695
khdip <khdip.ku@gmail.com>
2025-08-21 15:19:42 +0600

    My first angular project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, Header],
   template: `
     <h1>Welcome to {{title}}!</h1>
+    <p>Hello world!</p>
+    <app-header />
+    <app-home />
 
     <router-outlet />
   `,
-  styles: [],
+  styles: [`p {color:red}`],
 })
 export class App {
-  protected title = 'second-ng-app';
+  protected title = 'first-ng-app';
 }
